a54611b31afe8a0da5712ee7d7498c28b6a332f6
vatsalshah041 <113013039+vatsalshah041@users.noreply.github.com>
2022-09-24 01:15:07 +0530

    Update my.js

--- website 2.0/my.js
+++ website 2.0/my.js
@@ -16,8 +16,8 @@ function change2()
     if(document.getElementById('study').checked)
     {
         // console.log('hii');
-        document.getElementById('c2').innerHTML='My name is Vatsal Shah studying in DJ Sanghvi college of engineering.I am doing my Computer science engineering and am currently in second year of my studies.Excited to see what’s coming up in engineering.';
-        document.getElementById('c2').style.fontSize='20px';
+        document.getElementById('c2').innerHTML='I have done my primary and secondary studies from Universal High School(ICSE Board).Then I did my 11th and 12th through integrated studies.In 10th I scored 96.2%.In 12th I scored 91%.';
+                document.getElementById('c2').style.fontSize='20px';
         document.getElementById('c2').style.width='100%';
     }
     else{
